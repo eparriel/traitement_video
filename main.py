0ffcb8b835efecfe46b041481c2b3ddd114e9777
code-ex0 <louis.sasse@protonmail.com>
2021-12-15 22:45:53 +0100

    Changes by Louis

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+
+if __name__ == '__main__':
+    img = cv2.imread('interface_layout_ztpro.png')
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # mask of green (36,25,25) ~ (86, 255,255)
+    # mask = cv2.inRange(hsv, (36, 25, 25), (86, 255,255))
+    mask = cv2.inRange(hsv, (36, 25, 25), (70, 255, 255))
+
+    # slice the green
+    imask = mask>0
+    green = np.zeros_like(img, np.uint8)
+    green[imask] = img[imask]
+
+    # save
+    cv2.imwrite("only_points.png", green)
